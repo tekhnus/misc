052e15dcb8437e22aab46eba05629eaecd4e5db3
Zach Philipchook <z.pilipchuk@gmail.com>
2021-02-26 23:49:14 +0300

    Chess: fixup tests

--- funny/haskell-chess/test/Spec.hs
+++ funny/haskell-chess/test/Spec.hs
@@ -1,25 +1,13 @@
-import Control.Monad.Loops
 import Rules
 import Board
-
-playerTurn :: State -> IO State
-playerTurn state = do
-  print state
-  print (moves state)
-  turn <- readLn
-  case make turn state of
-    Just state' -> return state'
-    Nothing -> do
-      print "Wrong move, try again"
-      playerTurn state
 
 main :: IO ()
 main = do
     let (State _ newBoard) = newGame
-    print (moves newGame)
-    print (possibleMoves newGame (6, 0))
+    print (basicMoves newGame)
+    print (basicMovesFromPosition newGame (6, 0))
     print (movesAtDirection newGame (6, 0) 1 B)
-    print (makeMove ((6, 0), (5, 0)) newGame)
+    print (makeBasicMove ((6, 0), (5, 0)) newGame)
     print (moveFigure' (6, 0) (5, 0) newBoard)
     print (moveFigure (6, 0) (5, 0) newBoard)
     print (isKing (figureAt newBoard (5, 0)))
