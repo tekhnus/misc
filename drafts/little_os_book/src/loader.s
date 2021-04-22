a2a0f54b4e8564849a86511435ea4178fd11fc4a
Zach Philipchook <z.pilipchuk@gmail.com>
2021-04-22 16:10:09 +0300

    Comments

--- drafts/little_os_book/src/loader.s
+++ drafts/little_os_book/src/loader.s
@@ -1,17 +1,23 @@
-        global loader
+	;; Export the "loader" symbol
+	;; It will be configured as an entry point in the linker script
+	global loader
 
+	;; Define several constants for later use
         MAGIC_NUMBER equ 0x1BADB002
         FLAGS        equ 0x0
         CHECKSUM     equ -MAGIC_NUMBER
 
-
+	;; The code section starts
         section .text:
+	;; First comes the GRUB header.
+	;; It must be longword aligned.
         align 4
-                dd MAGIC_NUMBER
-                dd FLAGS
-                dd CHECKSUM
 
+        dd MAGIC_NUMBER
+	dd FLAGS
+        dd CHECKSUM
+	;; Next comes the loader code.
 loader:
-        mov eax, 0xCAFEBABE
-.loop:
+	mov eax, 0xCAFEBABE
+	.loop:
         jmp .loop
