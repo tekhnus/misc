b8e822e81846dbc3b06bbdeb9b591d8356d5be0f
Zach Philipchook <z.pilipchuk@gmail.com>
2021-04-23 13:24:53 +0300

    Cursor manipulation routines

--- drafts/little_os_book/src/kmain.c
+++ drafts/little_os_book/src/kmain.c
@@ -1,4 +1,12 @@
+#include "io.h"
+
 static char *const fb = ((char *const) 0x000B8000);
+
+const unsigned short INDEX_PORT = 0x3D4;
+const unsigned short DATA_PORT = 0x3D5;
+
+const char CURSOR_POS_HIGH = 14;
+const char CURSOR_POS_LOW = 15;
 
 void putchar(
 	     unsigned char row, unsigned char col,
@@ -9,10 +17,36 @@ void putchar(
   fb[loc + 1] = (bg << 4) | fg;
 }
 
+unsigned short find_cursor() {
+  unsigned short result = 0;
+  outb(INDEX_PORT, CURSOR_POS_HIGH);
+  result |= (inb(DATA_PORT) << 8);
+  outb(INDEX_PORT, CURSOR_POS_LOW);
+  result |= inb(DATA_PORT);
+  return result;
+}
+
+void move_cursor(unsigned char row, unsigned char col) {
+  unsigned short loc = 80 * row + col;
+
+  outb(INDEX_PORT, CURSOR_POS_HIGH);
+  outb(DATA_PORT, loc >> 8);
+  outb(INDEX_PORT, CURSOR_POS_LOW);
+  outb(DATA_PORT, loc);
+}
+
+void writechar(char c, unsigned char fg, unsigned char bg) {
+  unsigned short cur = find_cursor();
+  unsigned char row = cur / 80;
+  unsigned char col = cur % 80;
+  putchar(row, col, c, fg, bg);
+  move_cursor(row, col + 1);
+}
+
 void write(char *buf, unsigned int len) {
   unsigned int cnt;
   for (cnt = 0; cnt < len; ++cnt) {
-    putchar(0, cnt, buf[cnt], 15, 2);
+    writechar(buf[cnt], 15, 2);
   }
 }
 
--- drafts/little_os_book/src/loader.s
+++ drafts/little_os_book/src/loader.s
@@ -1,6 +1,8 @@
 	;; Export the "loader" symbol
 	;; It will be configured as an entry point in the linker script
 	global loader
+	global outb
+	global inb
 	;; Those are the names of C functions we'll use.
 	extern kmain
 
@@ -24,10 +26,21 @@ loader:
 	;; We make the esp point to the first
 	;; address after (= below) the segment reserved for the stack.
 	mov esp, kernel_stack + KERNEL_STACK_SIZE
+	;; TODO: shouldn't we also set ebp here?
 	;; Now we can call the C code!
 	call kmain
 	.loop:
         jmp .loop
+outb:			  ; void outb(unsigned short port, unsigned char data)
+	mov al, [esp + 8] ; data
+	mov dx, [esp + 4] ; port
+	out dx, al
+	ret
+inb:	      ; unsigned char inb(unsigned short port)
+	mov dx, [esp + 4]
+	xor eax, eax ; Just in case. TODO: find out if this is really needed
+	in al, dx
+	ret
 
 	section .bss
 	align 4
