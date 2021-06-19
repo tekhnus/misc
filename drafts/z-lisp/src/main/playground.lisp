90ff5be8f8b64b77c17f31cda5f4173c2c57b7be
Zach Philipchook <z.pilipchuk@gmail.com>
2021-06-20 00:00:05 +0300

    Remove the extra code from the repl

--- drafts/z-lisp/src/main/playground.lisp
+++ drafts/z-lisp/src/main/playground.lisp
@@ -6,6 +6,8 @@
 (def progn (builtin.fn (last args)))
 (def quote (builtin.form  (car args)))
 
+"hello, world!"
+
 (def second
      (builtin.fn
       (car (cdr (car args)))))
@@ -24,20 +26,32 @@
      (builtin.macro
       (deconsfn (head args) (second args))))
 
+(decons (a b c) '(7 8 9))
+a
+b
+c
+
 (def fn (builtin.macro `(builtin.fn (progn (decons ~(car args) args) ~(car (cdr args))))))
-
 (def macro (builtin.macro `(builtin.macro (progn (def ~(car args) args) ~(car (cdr args))))))
-
 (def form (builtin.macro `(builtin.form (progn (def ~(car args) args) ~(car (cdr args))))))
 
+(def twice (fn (arg) (add arg arg)))
+(twice 35)
+
+(progn 1 2 3)
 (def list (builtin.fn args))
+(list 1 2 3)
 
 (def defn (macro args `(def ~(car args) ~(cons 'fn (cdr args)))))
-
 (def defmacro (macro args `(def ~(car args) ~(cons 'macro (cdr args)))))
-
 (def defform (macro args `(def ~(car args) ~(cons 'form (cdr args)))))
 
+(defmacro addpi args (list 'add (car args) pi))
+(def pi 3)
+(addpi 8)
+
+(if (list pi) pi (add pi pi))
+(second '(1 2))
 (defn third args (head (tail (tail (head args)))))
 
 (defn map (f s)
@@ -58,6 +72,12 @@
 	x
 	(tail xs)))
     (list x)))
+
+(map (fn (arg) (add arg arg)) '(1 2 3 4 5))
+(append 5 '(1 2 3 4))
+
+(defn adder (n) (fn (m) (add n m)))
+((adder 3) 4)
 
 (def fopen
      (extern-pointer "libSystem.dylib" "fopen"
@@ -81,6 +101,8 @@
      (extern-pointer "libSystem.dylib" "fprintf"
 		((pointer string string) sizet)))
 
+
+
 (def stdin
      (extern-pointer "libSystem.dylib" "__stdinp" pointer))
 
@@ -89,6 +111,16 @@
 
 (def stderr
      (extern-pointer "libSystem.dylib" "__stderrp" pointer))
+
+(def hostsfile (fopen "/etc/hosts" "r"))
+(def buffer (malloc 2048))
+(fread buffer 1 1024 hostsfile)
+'(printfptr "%.2048s" buffer)
+'(fprintfstring stdout "%s" "Hello, world!")
+'(feof stdin)
+
+'(defn append (x ()) (list x) (x (cons head rest)) (cons head (append x rest)))
+'(defn reverse  (()) (list)  ((cons head rest)) (append head (reverse rest)))
 
 (defn repl (nsp)
   (progn
