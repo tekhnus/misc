f74d9ff7f7e4c9e53b2c253c73e553568d5efe4c
Zach Philipchook <z.pilipchuk@gmail.com>
2021-07-23 17:29:22 +0300

    Add tests

---
+++ drafts/euler-go/solutions/solutions_test.go
@@ -0,0 +1,23 @@
+package solutions
+
+import "testing"
+
+var tests = []struct {
+	N string
+	F func() int
+	V int
+}{
+	{"P1", P1, 233168},
+	{"P2", P2, 4613732},
+	{"P3", P3, 6857},
+}
+
+func TestSolutions(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.N, func(t *testing.T) {
+			if test.F() != test.V {
+				t.Fail()
+			}
+		})
+	}
+}
