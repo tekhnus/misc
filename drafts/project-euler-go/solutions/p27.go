66b86f6202e578b4e894e1499971581db1d8cf00
Zach Philipchook <z.pilipchuk@gmail.com>
2021-07-31 20:35:56 +0300

    27 refactoring

--- drafts/project-euler-go/solutions/p27.go
+++ drafts/project-euler-go/solutions/p27.go
@@ -3,32 +3,44 @@ package solutions
 import "github.com/tekhnus/project-euler-go"
 import "sync"
 
+type p27res struct{ I, J, N int64 }
+
+func (x p27res) CompareTo(y euler.Comparable) int {
+	return euler.CompareI64(x.N, y.(p27res).N)
+}
+
 func P27() int64 {
+	primec := make(chan int)
+	go euler.WritePrimes(primec)
+
+	isprime := make(map[int64]bool)
+	for p := range primec {
+		if p > 4000000 {
+			break
+		}
+		isprime[int64(p)] = true
+	}
+
 	pairs := make(chan euler.V2I64)
 	go euler.WritePairs(pairs, -999, 1000, -1000, 1001)
 
-	lengths := make(chan struct{ I, J, N int64 })
+	lengths := make(chan euler.Comparable, 4000000)
 	var wg sync.WaitGroup
-	euler.RunMany(2, &wg, func() {
+	euler.RunMany(3, &wg, func() {
 		for p := range pairs {
-			lengths <- struct{ I, J, N int64 }{p.I, p.J, primeCombo(p)}
+			n := primeCombo(p, isprime)
+			lengths <- p27res{p.I, p.J, n}
 		}
 	})
 	go euler.Closer(lengths, &wg)
-	var bestcombo int64
-	var bestproduct int64
-	for x := range lengths {
-		if x.N > bestcombo {
-			bestcombo = x.N
-			bestproduct = x.I * x.J
-		}
-	}
-	return bestproduct
+
+	best := euler.ReadMaxGeneric(lengths).(p27res)
+	return best.I * best.J
 }
 
-func primeCombo(coef euler.V2I64) int64 {
+func primeCombo(coef euler.V2I64, isprime map[int64]bool) int64 {
 	var n int64
-	for euler.IsPrime(int(n*n + coef.I*n + coef.J)) {
+	for isprime[n*n+coef.I*n+coef.J] {
 		n++
 	}
 	return n
