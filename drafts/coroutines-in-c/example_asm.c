90d6eccf22be4bfdaedf0d38c1aeee307b518df0
Zach Philipchook <z.pilipchuk@gmail.com>
2021-08-08 02:15:29 +0300

    First draft of asm-powered coroutines

---
+++ drafts/coroutines-in-c/example_asm.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char _Alignas(16) suspend_stack[1024 * 1024 * 32];
+void *suspend_rip;
+void *suspend_rsp = &suspend_stack[1024 * 1024 * 32 - 16];
+void *suspend_rbp;
+
+void swap(int *a, int *b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+void fib(void) {
+
+  asm(
+      "push +8(%rbp) \n"
+      "ret \n"
+      "resume_label2:"
+      );
+  int a = 0, b = 1;
+  for (;;) {
+    printf("%d\n", a);
+    a = a + b;
+    swap(&a, &b);
+    asm("call yield \n");
+  }
+}
+
+void *old_rsp;
+void *old_rbp;
+
+void init(void) {
+  asm(
+      "mov %%rsp, %0 \n"
+      "mov %%rbp, %1 \n"
+      : "=r"(old_rsp), "=r"(old_rbp)
+      : /* nope */
+      : /* nope */
+      );
+  asm(
+      "mov %0, %%rsp \n"
+      "mov %1, %%rbp \n"
+      : /* nope */
+      : "r"(suspend_rsp), "r"(suspend_rbp)
+      : /* nope */
+      );
+  asm("call _fib");
+  asm(
+        "fin:"      
+	"lea " "resume_label2" "(%%rip), %0 \n"
+	: "=r"(suspend_rip)
+	:
+	:
+	);
+    asm(
+	// Save rsp and rbp
+	"mov %%rsp, %0 \n"
+	"mov %%rbp, %1 \n"
+    
+	: "=r"(suspend_rsp), "=r"(suspend_rbp)
+	:
+	:
+	);
+    asm(
+	"mov %0, %%rsp \n"
+	"mov %1, %%rbp \n"
+	: /* nope */
+	: "r"(old_rsp), "r"(old_rbp)
+	: /* nope */
+	);
+}
+
+void resume(void) {
+  asm(
+      "mov %%rsp, %0 \n"
+      "mov %%rbp, %1 \n"
+      : "=r"(old_rsp), "=r"(old_rbp)
+      : /* nope */
+      : /* nope */
+      );
+  asm(
+      "mov %0, %%rsp \n"
+      "mov %1, %%rbp \n"
+      : /* nope */
+      : "r"(suspend_rsp), "r"(suspend_rbp)
+      : /* nope */
+      );
+  asm(
+      "jmp *%0 \n"
+      : /* nope */
+      : "r"(suspend_rip)
+      : /*nope*/
+      );
+  asm("yield:");
+      asm(
+	// Save rsp and rbp
+	"pop %2 \n"
+	"mov %%rsp, %0 \n"
+	"mov %%rbp, %1 \n"
+	: "=r"(suspend_rsp), "=r"(suspend_rbp), "=r"(suspend_rip)
+	:
+	:
+	);
+  asm(
+      "mov %0, %%rsp \n"
+      "mov %1, %%rbp \n"
+      : /* nope */
+      : "r"(old_rsp), "r"(old_rbp)
+      : /* nope */
+      );
+  //printf("still alive\n");
+}
+
+void bigfun() {
+  char bigarr[300000];
+  printf("%c\n", bigarr[300000 - 1]);
+}
+
+int main(void) {
+  init();
+  for (int i = 0; i < 10; ++i) {
+    resume();
+  }
+  //printf("hello, world!\n");
+  //bigfun();
+  return 0;
+}
