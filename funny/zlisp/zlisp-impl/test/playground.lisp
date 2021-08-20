f171b8a8b9e5d80c0992450fee85b160b420f8c5
Zach Philipchook <z.pilipchuk@gmail.com>
2021-08-21 00:03:44 +0300

    Fix the playground

--- funny/zlisp/zlisp-impl/test/playground.lisp
+++ funny/zlisp/zlisp-impl/test/playground.lisp
@@ -2,32 +2,25 @@
 
 (def args (list :p :q :r))
 
-(print (switch 42
-	    (33
-	     :one)
-	    (42
-	     :two)))
+!(#defun twice (arg) (+ arg arg))
 
-(def twice (fn (arg) (+ arg arg)))
 (print (twice 35))
 
 (print (list 1 2 3))
 
 (def pi 3)
-(defmacro addpi (arg) (list '+ arg pi))
-(print (addpi 8))
+
 
 (print (if (list pi) pi (add pi pi)))
 (print (second '(1 2)))
-(defn third args (head (tail (tail (head args)))))
+!(#defun third args (head (tail (tail (head args)))))
 
-(print (map (fn (arg) (+ arg arg)) '(1 2 3 4 5)))
 (print (append 5 '(1 2 3 4)))
 
-(defn adder (n) (fn (m) (+ n m)))
-(print ((adder 3) 4))
+'!(#defun adder (n) (fn (m) (+ n m)))
+'(print ((adder 3) 4))
 
-(def-or-panic printfptr
+!(#def-or-panica printfptr
      (extern-pointer libc "printf"
 		     '((string pointer) sizet)))
 
