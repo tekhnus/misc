163e965dc0f0bf2f335329c6197a0a48b592c09b
Zach Philipchook <z.pilipchuk@gmail.com>
2021-08-25 13:38:17 +0300

    Topo sort

--- drafts/algor/src/algor/__init__.py
+++ drafts/algor/src/algor/__init__.py
@@ -19,29 +19,35 @@ class GraphHashView:
         return self._graph[v]
 
 
-def dfs_recursive(g, s):
+def dfs_recursive(g, vs):
     visited = set()
 
     def _dfs(v):
-        yield v
+        yield ("enter", v)
         visited.add(v)
         for u in g.successors(v):
             if not u in visited:
                 yield from _dfs(u)
+        yield ("exit", v)
 
-    return _dfs(s)
+    while vs:
+        yield from _dfs(vs.pop())
 
 
-def dfs_iterative(g, v):
+def dfs_iterative(g, vs):
     visited = set()
-    st = collections.deque([v])
+    st = collections.deque([("enter", v) for v in vs])
     while st:
-        u = st.pop()
+        item = label, u = st.pop()
+        if label == "exit":
+            yield item
+            continue
         if u in visited:
             continue
-        yield u
+        yield item
         visited.add(u)
-        st.extend(reversed(g.successors(u)))
+        st.append(("exit", u))
+        st.extend(("enter", w) for w in reversed(g.successors(u)))
 
 
 def bfs(g, v):
@@ -54,3 +60,13 @@ def bfs(g, v):
         yield u
         visited.add(u)
         st.extend(g.successors(u))
+
+
+def topo_sort(vs, g):
+    """
+    If u ---> v and v -/-> u, then v will be outputted before u.
+    """
+    vs = set(vs)
+    for label, v in dfs_iterative(g, vs):
+        if label == "exit" and v in vs:
+            yield v
--- drafts/algor/test/test_algor.py
+++ drafts/algor/test/test_algor.py
@@ -13,14 +13,37 @@ g1 = (
 )
 
 
-@pytest.mark.parametrize("gr,exp", [(g1, [1, 2, 3, 4])])
+@pytest.mark.parametrize(
+    "gr,exp",
+    [
+        (
+            g1,
+            [
+                ("enter", 1),
+                ("enter", 2),
+                ("enter", 3),
+                ("enter", 4),
+                ("exit", 4),
+                ("exit", 3),
+                ("exit", 2),
+                ("exit", 1),
+            ],
+        )
+    ],
+)
 @pytest.mark.parametrize("f", [algor.dfs_recursive, algor.dfs_iterative])
 def test_dfs(gr, exp, f):
     g = algor.GraphHashView(algor.graph_list_to_hash(gr))
-    assert list(f(g, 1)) == exp
+    assert list(f(g, [1])) == exp
 
 
 @pytest.mark.parametrize("gr,exp", [(g1, [1, 2, 3, 4])])
 def test_bfs(gr, exp):
     g = algor.GraphHashView(algor.graph_list_to_hash(gr))
     assert list(algor.bfs(g, 1)) == exp
+
+
+@pytest.mark.parametrize("gr,vs,exp", [(g1, [3, 1, 2, 4], [4, 3, 2, 1])])
+def test_topo_sort(gr, vs, exp):
+    g = algor.GraphHashView(algor.graph_list_to_hash(gr))
+    assert list(algor.topo_sort(vs, g)) == exp
