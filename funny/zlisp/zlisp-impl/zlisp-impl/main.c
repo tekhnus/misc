aa498941273ebca708e411ad94b7565269d62022
Zach Philipchook <z.pilipchuk@gmail.com>
2022-01-06 15:42:28 +0300

    Remove the INtTERFACE hack, it is ugly

--- funny/zlisp/zlisp-impl/zlisp-impl/main.c
+++ funny/zlisp/zlisp-impl/zlisp-impl/main.c
@@ -3,8 +3,6 @@
 
 #include <ctype.h>
 #include <dlfcn.h>
-
-
 #include <libgen.h>
 
 #include <stddef.h>
@@ -12,11 +10,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if INTERFACE
-#include <inttypes.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <ffi.h>
 enum fdatumype {
   FDATUM_OK,
   FDATUM_PANIC,
@@ -26,136 +19,6 @@ enum fstate_type {
   FSTATE_OK,
   FSTATE_PANIC,
 };
-
-enum datum_type {
-  DATUM_NIL,
-  DATUM_LIST,
-  DATUM_SYMBOL,
-  DATUM_BYTESTRING,
-  DATUM_INTEGER,
-  DATUM_ROUTINE,
-  DATUM_POINTER,
-  DATUM_VOID,
-};
-
-struct routine {
-  prog *prog_;
-  state *state_;
-};
-
-struct datum {
-  enum datum_type type;
-  union {
-    struct {
-      datum *list_head;
-      datum *list_tail;
-    };
-    char *symbol_value;
-    char *bytestring_value;
-    int64_t integer_value;
-    routine routine_value;
-    struct {
-      void *pointer_value;
-      datum *pointer_descriptor;
-    };
-  };
-};
-
-struct state {
-  datum *vars;
-  datum *stack;
-  routine parent;
-  routine hat_parent;
-};
-
-enum read_result_type {
-  READ_RESULT_OK,
-  READ_RESULT_PANIC,
-  READ_RESULT_EOF,
-  READ_RESULT_RIGHT_PAREN,
-};
-
-struct read_result {
-  enum read_result_type type;
-  union {
-    datum *ok_value;
-    char *panic_message;
-  };
-};
-
-struct fdatum {
-  int type;
-  datum *ok_value;
-  char *panic_message;
-};
-
-struct fstate {
-  int type;
-  state *ok_value;
-  char *panic_message;
-};
-
-enum prog_type {
-  PROG_END,
-  PROG_IF,
-  PROG_NOP,
-  PROG_PUT_CONST,
-  PROG_PUT_ROUTINE,
-  PROG_PUT_VAR,
-  PROG_ARGS,
-  PROG_CALL,
-  PROG_COLLECT,
-  PROG_POP,
-  PROG_POP_PROG,
-  PROG_RETURN,
-  PROG_YIELD,
-  PROG_MODULE_END,
-};
-
-struct prog {
-  enum prog_type type;
-  union {
-    struct {
-      prog *if_true;
-      prog *if_false;
-    };
-    struct {
-      prog *nop_next;
-    };
-    struct {
-      datum *put_const_value;
-      prog *put_const_next;
-    };
-    struct {
-      datum *put_routine_value;
-      prog *put_routine_next;
-    };
-    struct {
-      datum *put_var_value;
-      prog *put_var_next;
-    };
-    struct prog *args_next;
-    struct {
-      bool call_hat;
-      prog *call_next;
-    };
-    struct prog *collect_next;
-    struct {
-      datum *pop_var;
-      prog *pop_next;
-    };
-    struct {
-      datum *pop_prog_var;
-      prog *pop_prog_next;
-    };
-    bool return_hat;
-    struct {
-      bool yield_hat;
-      prog *yield_next;
-    };
-  };
-};
-#endif
 
 LOCAL bool datum_is_the_symbol(datum *d, char *val) {
   return datum_is_symbol(d) && !strcmp(d->symbol_value, val);
--- funny/zlisp/zlisp-impl/zlisp-impl/main.h
+++ funny/zlisp/zlisp-impl/zlisp-impl/main.h
@@ -1,12 +1,112 @@
 /* This file was automatically generated.  Do not edit! */
 #undef INTERFACE
+typedef struct state state;
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <ffi.h>
-typedef struct state state;
-typedef struct fdatum fdatum;
 typedef struct datum datum;
+enum datum_type {
+  DATUM_NIL,
+  DATUM_LIST,
+  DATUM_SYMBOL,
+  DATUM_BYTESTRING,
+  DATUM_INTEGER,
+  DATUM_ROUTINE,
+  DATUM_POINTER,
+  DATUM_VOID,
+};
+typedef enum datum_type datum_type;
+typedef struct routine routine;
+typedef struct prog prog;
+enum prog_type {
+  PROG_END,
+  PROG_IF,
+  PROG_NOP,
+  PROG_PUT_CONST,
+  PROG_PUT_ROUTINE,
+  PROG_PUT_VAR,
+  PROG_ARGS,
+  PROG_CALL,
+  PROG_COLLECT,
+  PROG_POP,
+  PROG_POP_PROG,
+  PROG_RETURN,
+  PROG_YIELD,
+  PROG_MODULE_END,
+};
+typedef enum prog_type prog_type;
+struct prog {
+  enum prog_type type;
+  union {
+    struct {
+      prog *if_true;
+      prog *if_false;
+    };
+    struct {
+      prog *nop_next;
+    };
+    struct {
+      datum *put_const_value;
+      prog *put_const_next;
+    };
+    struct {
+      datum *put_routine_value;
+      prog *put_routine_next;
+    };
+    struct {
+      datum *put_var_value;
+      prog *put_var_next;
+    };
+    struct prog *args_next;
+    struct {
+      bool call_hat;
+      prog *call_next;
+    };
+    struct prog *collect_next;
+    struct {
+      datum *pop_var;
+      prog *pop_next;
+    };
+    struct {
+      datum *pop_prog_var;
+      prog *pop_prog_next;
+    };
+    bool return_hat;
+    struct {
+      bool yield_hat;
+      prog *yield_next;
+    };
+  };
+};
+struct routine {
+  prog *prog_;
+  state *state_;
+};
+struct datum {
+  enum datum_type type;
+  union {
+    struct {
+      datum *list_head;
+      datum *list_tail;
+    };
+    char *symbol_value;
+    char *bytestring_value;
+    int64_t integer_value;
+    routine routine_value;
+    struct {
+      void *pointer_value;
+      datum *pointer_descriptor;
+    };
+  };
+};
+struct state {
+  datum *vars;
+  datum *stack;
+  routine parent;
+  routine hat_parent;
+};
+typedef struct fdatum fdatum;
 struct fdatum {
   int type;
   datum *ok_value;
@@ -92,12 +192,6 @@ fdatum state_get_var(state *ns,datum *symbol);
 bool datum_is_routine(datum *e);
 fstate fstate_make_ok(state *v);
 fstate fstate_make_panic(char *message);
-typedef struct routine routine;
-typedef struct prog prog;
-struct routine {
-  prog *prog_;
-  state *state_;
-};
 fstate routine_run(routine c);
 void switch_context(routine *c,routine b,datum *v);
 bool datum_is_integer(datum *e);
@@ -145,108 +239,3 @@ state *state_make(datum *vars,datum *stack,routine parent,routine hat_parent);
 LOCAL void state_stack_put(state **ns,datum *value);
 bool datum_is_symbol(datum *e);
 LOCAL bool datum_is_the_symbol(datum *d,char *val);
-enum prog_type {
-  PROG_END,
-  PROG_IF,
-  PROG_NOP,
-  PROG_PUT_CONST,
-  PROG_PUT_ROUTINE,
-  PROG_PUT_VAR,
-  PROG_ARGS,
-  PROG_CALL,
-  PROG_COLLECT,
-  PROG_POP,
-  PROG_POP_PROG,
-  PROG_RETURN,
-  PROG_YIELD,
-  PROG_MODULE_END,
-};
-typedef enum prog_type prog_type;
-enum datum_type {
-  DATUM_NIL,
-  DATUM_LIST,
-  DATUM_SYMBOL,
-  DATUM_BYTESTRING,
-  DATUM_INTEGER,
-  DATUM_ROUTINE,
-  DATUM_POINTER,
-  DATUM_VOID,
-};
-typedef enum datum_type datum_type;
-struct datum {
-  enum datum_type type;
-  union {
-    struct {
-      datum *list_head;
-      datum *list_tail;
-    };
-    char *symbol_value;
-    char *bytestring_value;
-    int64_t integer_value;
-    routine routine_value;
-    struct {
-      void *pointer_value;
-      datum *pointer_descriptor;
-    };
-  };
-};
-struct state {
-  datum *vars;
-  datum *stack;
-  routine parent;
-  routine hat_parent;
-};
-struct prog {
-  enum prog_type type;
-  union {
-    struct {
-      prog *if_true;
-      prog *if_false;
-    };
-    struct {
-      prog *nop_next;
-    };
-    struct {
-      datum *put_const_value;
-      prog *put_const_next;
-    };
-    struct {
-      datum *put_routine_value;
-      prog *put_routine_next;
-    };
-    struct {
-      datum *put_var_value;
-      prog *put_var_next;
-    };
-    struct prog *args_next;
-    struct {
-      bool call_hat;
-      prog *call_next;
-    };
-    struct prog *collect_next;
-    struct {
-      datum *pop_var;
-      prog *pop_next;
-    };
-    struct {
-      datum *pop_prog_var;
-      prog *pop_prog_next;
-    };
-    bool return_hat;
-    struct {
-      bool yield_hat;
-      prog *yield_next;
-    };
-  };
-};
-enum fstate_type {
-  FSTATE_OK,
-  FSTATE_PANIC,
-};
-typedef enum fstate_type fstate_type;
-enum fdatumype {
-  FDATUM_OK,
-  FDATUM_PANIC,
-};
-typedef enum fdatumype fdatumype;
-#define INTERFACE 0
