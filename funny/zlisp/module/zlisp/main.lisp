bbe3a981b10e7099a08bf15018a5e9fe1c132fe6
Zach Philipchook <z.pilipchuk@gmail.com>
2022-01-07 12:16:48 +0300

    Wrapped all c functions in routines

--- funny/zlisp/module/zlisp/main.lisp
+++ funny/zlisp/module/zlisp/main.lisp
@@ -5,11 +5,14 @@
   (shared-library "libzlisp-impl-wrapper.so")
   (shared-library "libzlisp-impl-wrapper.dylib"))
 
-!(#def-or-panica read
+!(#def-or-panica read-
      (extern-pointer zlisp-zlisp "read" '((datum) val)))
+!(#wrap-fn-pointer read read-)
 
-!(#def-or-panica eval
+!(#def-or-panica eval-
      (extern-pointer zlisp-zlisp "eval" '((datum datum) val)))
+!(#wrap-fn-pointer eval eval-)
 
-!(#def-or-panica builtins
+!(#def-or-panica builtins-
   (extern-pointer zlisp-zlisp "builtins" '(() val)))
+!(#wrap-fn-pointer builtins builtins-)
--- funny/zlisp/zlisp-cli-bootstrap/test/print-hosts.lisp
+++ funny/zlisp/zlisp-cli-bootstrap/test/print-hosts.lisp
@@ -1,9 +1,10 @@
 !(require "stdmacro")
 (require "libc")
 
-!(#def-or-panica printfptr
+!(#def-or-panica printfptr-
    (extern-pointer libc "printf"
 		   '((string pointer) sizet)))
+!(#wrap-fn-pointer printfptr printfptr-)
 (def hostsfile (fopen "/etc/hosts" "r"))
 (def buffer (malloc 2048))
 (ignore (fread buffer 1 1024 hostsfile))
