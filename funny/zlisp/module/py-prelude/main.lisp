942debe41c0be6d0b165b02d357b12ff8921b220
Zach Philipchook <z.pilipchuk@gmail.com>
2022-04-30 15:02:13 +0300

    continue

--- funny/zlisp/module/py-prelude/main.lisp
+++ funny/zlisp/module/py-prelude/main.lisp
@@ -3,3 +3,9 @@
 (builtin.defn head (return (host ("eval" "head(x)") args)))
 
 (builtin.defn tail (return (host ("eval" "tail(x)") args)))
+
+(builtin.defn is-constant (return (host ("eval" "is_constant(x)") args)))
+
+(builtin.defn eq (return (host ("eval" "eq(x)") args)))
+
+(builtin.defn panic (return (host ("eval" "panic(x)") args)))
