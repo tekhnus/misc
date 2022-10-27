d25750d7bef5fb794b27357de8b6cf8f3b5d5eba
Zach Philipchook <z.pilipchuk@gmail.com>
2022-10-27 23:07:35 +0300

    clean up the builder source

--- funny/zlisp/builder/main.c
+++ funny/zlisp/builder/main.c
@@ -31,17 +31,10 @@ int main(int argc, char **argv) {
   // so the first call of append_new must be for the starting point.
   size_t bp = prog_slice_append_new(&sl);
   size_t p = prog_slice_append_new(&sl);
-  // datum *s = routine_2_make(bp);
   datum *compdata = compdata_make();
   datum *builder_compdata = compdata_make();
   prog_build_init(&sl, &p, &bp, &compdata, &builder_compdata);
-  fdatum bytecode = prog_compile(src.ok_value, &compdata, datum_make_list_1(datum_make_symbol("main")));
-  if (fdatum_is_panic(bytecode)) {
-    fprintf(stderr, "%s\n", bytecode.panic_message);
-    return EXIT_FAILURE;
-  }
-  char *err = relocate_and_build(&sl, &p, &bp, bytecode.ok_value, compile_module, &builder_compdata, datum_make_bytestring(argv[1]));
-
+  char *err = prog_build(&sl, &p, &bp, src.ok_value, &compdata, &builder_compdata, datum_make_bytestring(argv[1]));
   if (err != NULL) {
     fprintf(stderr, "compilation error: %s\n", err);
     return EXIT_FAILURE;
@@ -50,12 +43,29 @@ int main(int argc, char **argv) {
   return EXIT_SUCCESS;
 }
 
+EXPORT char *prog_build(prog_slice *sl, size_t *p, size_t *bp, datum *source, datum **compdata, datum **builder_compdata, datum *settings) {
+  fdatum bytecode = prog_compile(source, compdata, datum_make_list_1(datum_make_symbol("main")));
+  if (fdatum_is_panic(bytecode)) {
+    return bytecode.panic_message;
+  }
+  prog_append_nop(sl, p, datum_make_symbol("this_is_so_that_relocation_is_possible"));
+  size_t start_p = *p;
+  char *res = prog_slice_relocate(sl, p, bytecode.ok_value);
+  if (res != NULL) {
+    return res;
+  }
+  return prog_link_deps(sl, bp, builder_compdata, start_p, compile_module, settings);
+}
+
 LOCAL fdatum compile_module(char *module, datum *settings) {
   if (!datum_is_bytestring(settings)) {
     return fdatum_make_panic("settings should be a string");
   }
-  settings = settings;
-  fdatum src = preprocessed_module_source(module, settings->bytestring_value);
+  if (!strcmp(module, "prelude")) {
+    module = settings->bytestring_value;
+  }
+  char *fname = module_to_filename(module);
+  fdatum src = file_source(fname);
   if (fdatum_is_panic(src)) {
     return src;
   }
@@ -63,30 +73,47 @@ LOCAL fdatum compile_module(char *module, datum *settings) {
   return prog_compile(src.ok_value, &compdata, datum_make_list_1(datum_make_symbol(module)));
 }
 
-LOCAL fdatum preprocessed_module_source(char *module, char *prelude_module_name) {
-  if (!strcmp(module, "prelude")) {
-    module = prelude_module_name;
+LOCAL fdatum file_source(char *fname) {
+  FILE *stre = fopen(fname, "r");
+  if (stre == NULL) {
+    char *err = malloc(1024);
+    err[0] = 0;
+    strcat(err, "Module not found: ");
+    strcat(err, fname);
+    return fdatum_make_panic(err);
   }
-  char *fname = module_to_filename(module);
-  return file_source(fname);
-}
 
-char *relocate_and_build(prog_slice *sl, size_t *ep, size_t *bdr_p, datum *bytecode, fdatum (*module_bytecode)(char *, datum *), datum **builder_compdata, datum *settings) {
-  prog_append_nop(sl, ep, datum_make_symbol("this_is_so_that_relocation_is_possible"));
-  size_t original_ep = *ep;
-  char *res = prog_slice_relocate(sl, ep, bytecode);
-  if (res != NULL) {
-    return res;
+  prog_slice expander_sl = prog_slice_make(16 * 1024);
+  size_t expander_prg = prog_slice_append_new(&expander_sl);
+  size_t expander_builder_prg = prog_slice_append_new(&expander_sl);
+  datum *expander_routine = routine_2_make(expander_builder_prg);
+  datum *expander_compdata = compdata_make();
+  datum *expander_builder_compdata = compdata_make();
+  prog_build_init(&expander_sl, &expander_prg, &expander_builder_prg, &expander_compdata, &expander_builder_compdata);
+  read_result rr;
+  datum *res = datum_make_nil();
+  datum **resend = &res;
+  for (; read_result_is_ok(rr = datum_read(stre));) {
+    fdatum val = datum_expand(rr.ok_value, &expander_sl, &expander_routine, &expander_prg, &expander_compdata, &expander_builder_prg, &expander_builder_compdata);
+    if (fdatum_is_panic(val)) {
+      char *err = malloc(1024);
+      char *end = err;
+      end += sprintf(end, "while expanding %s: %s", datum_repr(rr.ok_value), val.panic_message);
+      return fdatum_make_panic(err);
+    }
+    if (datum_is_the_symbol(val.ok_value, ":void-value")) {
+      continue;
+    }
+    *resend = datum_make_list(val.ok_value, datum_make_nil());
+    resend = &((*resend)->list_tail);
   }
-  return prog_link_deps(sl, bdr_p, builder_compdata, original_ep, module_bytecode, settings);
-}
-
-char *prog_build_c_host(prog_slice *sl, size_t *p, size_t *bp, datum *source, datum **compdata, datum **builder_compdata) {
-  fdatum bytecode = prog_compile(source, compdata, datum_make_list_1(datum_make_symbol("main")));
-  if (fdatum_is_panic(bytecode)) {
-    return bytecode.panic_message;
+  if (read_result_is_panic(rr)) {
+    return fdatum_make_panic(rr.panic_message);
   }
-  return relocate_and_build(sl, p, bp, bytecode.ok_value, module_routine, builder_compdata, datum_make_bytestring("c-prelude"));
+  if (read_result_is_right_paren(rr)) {
+    return fdatum_make_panic("unmatched right paren");
+  }
+  return fdatum_make_ok(res);
 }
 
 LOCAL fdatum datum_expand(datum *e, prog_slice *sl, datum **routine, size_t *p, datum **compdata, size_t *bp, datum **builder_compdata) {
@@ -116,8 +143,8 @@ LOCAL fdatum datum_expand(datum *e, prog_slice *sl, datum **routine, size_t *p, 
   if (fdatum_is_panic(exp)) {
     return exp;
   }
-  char *err = prog_build_c_host(
-                                  sl, p, bp, datum_make_list_1(exp.ok_value), compdata, builder_compdata);
+  char *err = prog_build(
+                                sl, p, bp, datum_make_list_1(exp.ok_value), compdata, builder_compdata, datum_make_bytestring("c-prelude"));
   if (err != NULL) {
     char *err2 = malloc(256);
     err2[0] = 0;
@@ -129,36 +156,13 @@ LOCAL fdatum datum_expand(datum *e, prog_slice *sl, datum **routine, size_t *p, 
   if (fdatum_is_panic(res)) {
     return res;
   }
-  //size_t p_alt = routine_2_get_offset(*routine);
-  /* if (!fdatum_is_panic(res) && p_alt != *p) { */
-  /*   return fdatum_make_panic("suddenly p_alt != p"); */
-  /* } */
   if (!compdata_has_value(*compdata)) {
     return fdatum_make_ok(datum_make_symbol(":void-value"));
   }
   return res;
 }
 
-
-fdatum module_routine(char *module, datum *settings) {
-  settings = settings;
-  fdatum src = module_source(module);
-  if (fdatum_is_panic(src)) {
-    return src;
-  }
-  datum *compdata = compdata_make();
-  return prog_compile(src.ok_value, &compdata, datum_make_list_1(datum_make_symbol(module)));
-}
-
-fdatum module_source(char *module) {
-  if (!strcmp(module, "prelude")) {
-    module = "c-prelude";
-  }
-  char *fname = module_to_filename(module);
-  return file_source(fname);
-}
-
-char *module_to_filename(char *module) {
+LOCAL char *module_to_filename(char *module) {
   char *fname = malloc(1024);
   char *zlisp_home = getenv("ZLISP");
   if (zlisp_home == NULL) {
@@ -170,55 +174,5 @@ char *module_to_filename(char *module) {
   strcat(fname, module);
   strcat(fname, "/main.lisp");
   return fname;
-}
-
-fdatum file_source(char *fname) {
-  FILE *stre = fopen(fname, "r");
-  if (stre == NULL) {
-    char *err = malloc(1024);
-    err[0] = 0;
-    strcat(err, "Module not found: ");
-    strcat(err, fname);
-    return fdatum_make_panic(err);
-  }
-
-  prog_slice expander_sl = prog_slice_make(16 * 1024);
-  size_t expander_prg = prog_slice_append_new(&expander_sl);
-  size_t expander_builder_prg = prog_slice_append_new(&expander_sl);
-  datum *expander_routine = routine_2_make(expander_builder_prg);
-  datum *expander_compdata = compdata_make();
-  datum *expander_builder_compdata = compdata_make();
-  prog_build_init(&expander_sl, &expander_prg, &expander_builder_prg, &expander_compdata, &expander_builder_compdata);
-  // routine_run_and_get_value_c_host_new(expander_sl, &expander_routine);
-  read_result rr;
-  datum *res = datum_make_nil();
-  datum **resend = &res;
-  // printf("start expanding %s\n", fname);
-  for (; read_result_is_ok(rr = datum_read(stre));) {
-    // printf("preparing to expand a statement\n");
-    fdatum val = datum_expand(rr.ok_value, &expander_sl, &expander_routine, &expander_prg, &expander_compdata, &expander_builder_prg, &expander_builder_compdata);
-    // printf("expanded a statement\n");
-    if (fdatum_is_panic(val)) {
-      char *err = malloc(1024);
-      char *end = err;
-      end += sprintf(end, "while expanding %s: %s", datum_repr(rr.ok_value), val.panic_message);
-      return fdatum_make_panic(err);
-    }
-    // printf("%s\n", datum_repr(val.ok_value));
-    if (datum_is_the_symbol(val.ok_value, ":void-value")) {
-      // to support things like !(def x 42)
-      continue;
-    }
-    *resend = datum_make_list(val.ok_value, datum_make_nil());
-    resend = &((*resend)->list_tail);
-  }
-  // printf("expanded all statements in %s\n", fname);
-  if (read_result_is_panic(rr)) {
-    return fdatum_make_panic(rr.panic_message);
-  }
-  if (read_result_is_right_paren(rr)) {
-    return fdatum_make_panic("unmatched right paren");
-  }
-  return fdatum_make_ok(res);
 }
 
--- funny/zlisp/builder/main.h
+++ funny/zlisp/builder/main.h
@@ -2,14 +2,11 @@
 #undef INTERFACE
 #include <zlisp/common.h>
 #include <zlisp/host-ffi.h>
-fdatum module_source(char *module);
 #define LOCAL static
 LOCAL fdatum datum_expand(datum *e,prog_slice *sl,datum **routine,size_t *p,datum **compdata,size_t *bp,datum **builder_compdata);
-fdatum module_routine(char *module,datum *settings);
-char *prog_build_c_host(prog_slice *sl,size_t *p,size_t *bp,datum *source,datum **compdata,datum **builder_compdata);
-char *module_to_filename(char *module);
-LOCAL fdatum preprocessed_module_source(char *module,char *prelude_module_name);
+LOCAL char *module_to_filename(char *module);
 LOCAL fdatum compile_module(char *module,datum *settings);
-char *relocate_and_build(prog_slice *sl,size_t *ep,size_t *bdr_p,datum *bytecode,fdatum(*module_bytecode)(char *,datum *),datum **builder_compdata,datum *settings);
-fdatum file_source(char *fname);
+#define EXPORT
+char *prog_build(prog_slice *sl,size_t *p,size_t *bp,datum *source,datum **compdata,datum **builder_compdata,datum *settings);
+LOCAL fdatum file_source(char *fname);
 #define INTERFACE 0
