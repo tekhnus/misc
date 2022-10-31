0caa74207f66a217f426cb8f3ca007bf589fea45
Zach Philipchook <z.pilipchuk@gmail.com>
2022-11-01 00:39:26 +0300

    defun = builtin.defun

--- funny/zlisp/module/defun/main.lisp
+++ funny/zlisp/module/defun/main.lisp
@@ -3,7 +3,7 @@
  (tail "prelude" tail)
  (switch-fun "std-pre-defun" switch-fun))
 
-(def defun (builtin.fn (x y z) (return `(builtin.defn ~x ~(switch-fun `((~y ~z)))))))
+(def defun (builtin.fn (x y z) (return `(builtin.defn ~x ~y ~z))))
 
 
 
