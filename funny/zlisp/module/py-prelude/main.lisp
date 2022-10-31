13930647aa7987480989765e2a7a8dc61aaa11af
Zach Philipchook <z.pilipchuk@gmail.com>
2022-11-01 00:25:43 +0300

    more

--- funny/zlisp/module/py-prelude/main.lisp
+++ funny/zlisp/module/py-prelude/main.lisp
@@ -1,28 +1,28 @@
 (req)
 
-(builtin.defn print (return (host ("eval" "print(x[1][0])") args)))
+(builtin.defn print (a0) (return (host ("eval" "print(x[1][0])") `(~a0))))
 
-(builtin.defn debug-print (return (host ("eval" "print(x[1][0])") args)))
+(builtin.defn debug-print (a0) (return (host ("eval" "print(x[1][0])") `(~a0))))
 
-(builtin.defn head (return (host ("eval" "head(x)") args)))
+(builtin.defn head (a0) (return (host ("eval" "head(x)") `(~a0))))
 
-(builtin.defn tail (return (host ("eval" "tail(x)") args)))
+(builtin.defn tail (a0) (return (host ("eval" "tail(x)") `(~a0))))
 
-(builtin.defn is-constant (return (host ("eval" "is_constant(x)") args)))
+(builtin.defn is-constant (a0) (return (host ("eval" "is_constant(x)") `(~a0))))
 
-(builtin.defn eq (return (host ("eval" "eq(x)") args)))
+(builtin.defn eq (a0 a1) (return (host ("eval" "eq(x)") `(~a0 ~a1))))
 
-(builtin.defn panic (return (host ("eval" "panic(x)") args)))
+(builtin.defn panic (a0) (return (host ("eval" "panic(x)") `(~a0))))
 
-(builtin.defn annotate (return (host ("eval" "annotate(x)") args)))
+(builtin.defn annotate (a0) (return (host ("eval" "annotate(x)") `(~a0))))
 
-(builtin.defn + (return (host ("eval" "add(x)") args)))
+(builtin.defn + (a0 a1) (return (host ("eval" "add(x)") `(~a0 ~a1))))
 
-(builtin.defn cons (return (host ("eval" "cons(x)") args)))
+(builtin.defn cons (a0 a1) (return (host ("eval" "cons(x)") `(~a0 ~a1))))
 
-(builtin.defn concat-bytestrings (return (host ("eval" "concat_bytestrings(x)") args)))
+(builtin.defn concat-bytestrings (a0 a1) (return (host ("eval" "concat_bytestrings(x)") `(~a0 ~a1))))
 
-(builtin.defn repr (return (host ("eval" "repre(x)") args)))
+(builtin.defn repr (a0) (return (host ("eval" "repre(x)") `(~a0))))
 
 (export
  (print print)
