847c3aeb620fac51e14e475e7811596b988c9169
Zach Philipchook <z.pilipchuk@gmail.com>
2023-01-05 00:37:36 +0200

    migrate py-prelude to defun

--- funny/zlisp/module/py-prelude/main.lisp
+++ funny/zlisp/module/py-prelude/main.lisp
@@ -1,28 +1,28 @@
 (req)
 
-(builtin.defn print (a0) (return (host ("eval" "print(x[1][0])") `(~a0))))
+(builtin.defun print (a0) (return (host ("eval" "print(x[1][0])") `(~a0))))
 
-(builtin.defn debug-print (a0) (return (host ("eval" "print(x[1][0])") `(~a0))))
+(builtin.defun debug-print (a0) (return (host ("eval" "print(x[1][0])") `(~a0))))
 
-(builtin.defn head (a0) (return (host ("eval" "head(x)") `(~a0))))
+(builtin.defun head (a0) (return (host ("eval" "head(x)") `(~a0))))
 
-(builtin.defn tail (a0) (return (host ("eval" "tail(x)") `(~a0))))
+(builtin.defun tail (a0) (return (host ("eval" "tail(x)") `(~a0))))
 
-(builtin.defn is-constant (a0) (return (host ("eval" "is_constant(x)") `(~a0))))
+(builtin.defun is-constant (a0) (return (host ("eval" "is_constant(x)") `(~a0))))
 
-(builtin.defn eq (a0 a1) (return (host ("eval" "eq(x)") `(~a0 ~a1))))
+(builtin.defun eq (a0 a1) (return (host ("eval" "eq(x)") `(~a0 ~a1))))
 
-(builtin.defn panic (a0) (return (host ("eval" "panic(x)") `(~a0))))
+(builtin.defun panic (a0) (return (host ("eval" "panic(x)") `(~a0))))
 
-(builtin.defn annotate (a0) (return (host ("eval" "annotate(x)") `(~a0))))
+(builtin.defun annotate (a0) (return (host ("eval" "annotate(x)") `(~a0))))
 
-(builtin.defn + (a0 a1) (return (host ("eval" "add(x)") `(~a0 ~a1))))
+(builtin.defun + (a0 a1) (return (host ("eval" "add(x)") `(~a0 ~a1))))
 
-(builtin.defn cons (a0 a1) (return (host ("eval" "cons(x)") `(~a0 ~a1))))
+(builtin.defun cons (a0 a1) (return (host ("eval" "cons(x)") `(~a0 ~a1))))
 
-(builtin.defn concat-bytestrings (a0 a1) (return (host ("eval" "concat_bytestrings(x)") `(~a0 ~a1))))
+(builtin.defun concat-bytestrings (a0 a1) (return (host ("eval" "concat_bytestrings(x)") `(~a0 ~a1))))
 
-(builtin.defn repr (a0) (return (host ("eval" "repre(x)") `(~a0))))
+(builtin.defun repr (a0) (return (host ("eval" "repre(x)") `(~a0))))
 
 (export
  (print print)
