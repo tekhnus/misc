5de9cfe1eb271a1c6bd1101adc87c4a8ee214f5e
Zach Philipchook <z.pilipchuk@gmail.com>
2023-01-21 22:48:16 +0200

    remove frame height in favor of type_id

--- funny/zlisp/core/running.c
+++ funny/zlisp/core/running.c
@@ -69,7 +69,6 @@ struct prog {
 
 struct frame {
   vec state;
-  int height;
   datum type_id;
   datum parent_type_id;
 };
@@ -422,7 +421,6 @@ LOCAL void routine_copy(routine *dst, routine *src) {
 
 LOCAL void frame_copy(frame *dst, frame *src) {
   vec_copy(&dst->state, &src->state);
-  dst->height = src->height;
   dst->type_id = src->type_id;
   dst->parent_type_id = src->parent_type_id;
 }
@@ -458,21 +456,22 @@ LOCAL datum *routine_get_shape(routine *r) {
 }
 
 LOCAL routine *routine_merge(routine *r, routine *rt_tail) {
-  assert(r->frames[0]->height == 0);
   assert(routine_get_count(rt_tail) > 0);
-  size_t tail_height = rt_tail->frames[0]->height;
-  if (tail_height >= routine_get_count(r) + 1) {
-    fprintf(stderr, "routine_merge: not enough variables\n");
-    exit(EXIT_FAILURE);
-  }
+  datum *tail_parent_type = &rt_tail->frames[0]->parent_type_id;
   routine *rt = malloc(sizeof(routine));
   rt->cnt = 0;
-  for (size_t height = 0; height < tail_height; ++height) {
-    assert(r->frames[height]->height == (int)height);
+  for (size_t height = 0; height < routine_get_count(r); ++height) {
+    if (datum_is_nil(tail_parent_type) || (rt->cnt > 0 && datum_eq(tail_parent_type, &rt->frames[rt->cnt - 1]->type_id))) {
+      break;
+    }
     rt->frames[rt->cnt++] = r->frames[height];
   }
+  if (datum_is_nil(tail_parent_type)) {
+    assert(rt->cnt == 0);
+  } else {
+    assert(rt->cnt > 0 && datum_eq(&rt->frames[rt->cnt - 1]->type_id, tail_parent_type));
+  }
   for (size_t j = 0; j <= routine_get_count(rt_tail); ++j) {
-    assert(rt_tail->frames[j]->height == (int)j + (int)tail_height);
     rt->frames[rt->cnt++] = rt_tail->frames[j];
   }
   return rt;
@@ -488,14 +487,12 @@ LOCAL routine *routine_make_empty(ptrdiff_t prg, routine *context) {
   r->cnt = 2;
   r->frames[0] = malloc(sizeof(struct frame));
   r->frames[0]->state = vec_make(1024);
-  r->frames[0]->height = context != NULL ? routine_get_count(context) : 0;
   r->frames[0]->type_id = *datum_make_int(prg);
   assert(context == NULL || routine_get_count(context) > 0);
   r->frames[0]->parent_type_id = context != NULL ? *datum_copy(&context->frames[routine_get_count(context) - 1]->type_id) : *datum_make_nil();
   r->frames[1] = malloc(sizeof(struct frame));
   r->frames[1]->state = vec_make(1);
   vec_append(&r->frames[1]->state, *datum_make_int(prg));
-  r->frames[1]->height = r->frames[0]->height + 1;
   r->frames[1]->type_id = *datum_make_nil();
   r->frames[1]->parent_type_id = *datum_copy(&r->frames[0]->type_id);
   return r;
