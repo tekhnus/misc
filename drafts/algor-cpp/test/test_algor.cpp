ae88c0c15c0ce4201c8beb197fe64f3f6864bb7d
Zach Philipchook <z.pilipchuk@gmail.com>
2023-04-17 15:39:22 +0300

    explicit std:: in tests

--- drafts/algor-cpp/test/test_algor.cpp
+++ drafts/algor-cpp/test/test_algor.cpp
@@ -5,23 +5,21 @@
 #include <sstream>
 #include <variant>
 
-using namespace std;
-
-vector<int> vs{1, 2, 3, 4};
-vector<pair<string, pair<int, int>>> es{
+std::vector<int> vs{1, 2, 3, 4};
+std::vector<std::pair<std::string, std::pair<int, int>>> es{
     {"a", {1, 2}},
     {"b", {1, 3}},
     {"c", {2, 3}},
     {"d", {3, 4}},
 };
-graph<int, string> g;
-map<string, int> w = {
+graph<int, std::string> g;
+std::map<std::string, int> w = {
     {"a", 3},
     {"b", 1},
     {"c", 7},
     {"d", 5},
 };
-map<pair<int, int>, tuple<int, optional<string>>> exp_dist = {
+std::map<std::pair<int, int>, std::tuple<int, std::optional<std::string>>> exp_dist = {
     {{1, 1}, {0, {}}},   {{1, 2}, {3, "a"}}, {{1, 3}, {1, "b"}},
     {{1, 4}, {6, "d"}},  {{2, 2}, {0, {}}},  {{2, 3}, {7, "c"}},
     {{2, 4}, {12, "d"}}, {{3, 3}, {0, {}}},  {{3, 4}, {5, "d"}},
@@ -31,9 +29,9 @@ map<pair<int, int>, tuple<int, optional<string>>> exp_dist = {
 template <typename T, typename dummy = void>
 void assert_equal(T const &a, T const &b) {
   if (a != b) {
-    stringstream s;
+    std::stringstream s;
     s << &a << " != " << &b;
-    throw runtime_error(s.str());
+    throw std::runtime_error(s.str());
   }
 }
 
@@ -50,29 +48,29 @@ void assert_equal(T const &a, T const &b) {
 
 void test_dfs() {
   auto d = dfs(vs.begin(), vs.begin() + 1, g);
-  vector<variant<dfs_enter<int, string>, dfs_exit<int>, dfs_end>> res;
-  generate_n(back_inserter(res), 9, ref(d));
-  vector<variant<dfs_enter<int, string>, dfs_exit<int>, dfs_end>> exp = {
-      {dfs_enter<int, string>{1, {}}, dfs_enter<int, string>{2, "a"},
-       dfs_enter<int, string>{3, "c"}, dfs_enter<int, string>{4, "d"},
+  std::vector<std::variant<dfs_enter<int, std::string>, dfs_exit<int>, dfs_end>> res;
+  std::generate_n(std::back_inserter(res), 9, std::ref(d));
+  std::vector<std::variant<dfs_enter<int, std::string>, dfs_exit<int>, dfs_end>> exp = {
+      {dfs_enter<int, std::string>{1, {}}, dfs_enter<int, std::string>{2, "a"},
+       dfs_enter<int, std::string>{3, "c"}, dfs_enter<int, std::string>{4, "d"},
        dfs_exit<int>{4}, dfs_exit<int>{3}, dfs_exit<int>{2}, dfs_exit<int>{1},
        dfs_end{}}};
   assert_equal(res, res);
 }
 
 void test_topo_sort() {
-  vector<int> res;
+  std::vector<int> res;
   topo_sort(back_inserter(res), vs.begin(), vs.end(), g);
   assert_equal(res, {4, 3, 2, 1});
 }
 
 void test_strong_components() {
-  map<int, int> res;
+  std::map<int, int> res;
   strong_components(inserter(res, res.end()), vs.begin(), vs.end(), g);
   assert_equal(res, {{1, 1}, {2, 1}, {3, 1}, {4, 1}});
 }
 
-string val_of(string *s) {
+std::string val_of(std::string *s) {
   if (s == nullptr) {
     return "(null string)";
   }
@@ -80,9 +78,9 @@ string val_of(string *s) {
 }
 
 void test_ford_bellman() {
-  map<int, int> best;
-  map<int, variant<string, root, unreachable>> pred;
-  vector<int> start{1};
+  std::map<int, int> best;
+  std::map<int, std::variant<std::string, root, unreachable>> pred;
+  std::vector<int> start{1};
   ford_bellman(best, pred, start.begin(), start.end(), g, w);
 
   for (auto &u : vs) {
@@ -101,20 +99,20 @@ void test_ford_bellman() {
 
 void test_dijkstra() {
   auto d = dijkstra(vs.begin(), vs.begin() + 1, g, w);
-  vector<optional<tuple<int, tuple<int, string>>>> res;
-  generate_n(std::back_inserter(res), 5, ref(d));
+  std::vector<std::optional<std::tuple<int, std::tuple<int, std::string>>>> res;
+  generate_n(std::back_inserter(res), 5, std::ref(d));
   for (auto item : res) {
     if (!item.has_value()) {
       continue;
     }
     auto [v, we] = item.value();
     auto [w, e] = we;
-    assert_equal(exp_dist[{1, v}], tuple<int, optional<string>>{w, e});
+    assert_equal(exp_dist[{1, v}], std::tuple<int, std::optional<std::string>>{w, e});
   }
 }
 
 void test_pairwise_distances() {
-  vector<int> start{1};
+  std::vector<int> start{1};
   auto d = pairwise_distances(g, w);
   for (auto &v : vs) {
     for (auto &u : vs) {
@@ -128,7 +126,7 @@ void test_pairwise_distances() {
 }
 
 void test_floyd_warshall() {
-  vector<int> start{1};
+  std::vector<int> start{1};
   auto [d, pred] = floyd_warshall(g, w);
   for (auto &v : vs) {
     for (auto &u : vs) {
@@ -144,14 +142,14 @@ void test_floyd_warshall() {
   }
 }
 
-void run_test(string name, void (*test)()) {
+void run_test(std::string name, void (*test)()) {
   try {
     test();
   } catch (...) {
-    cout << "FAIL " << name << endl;
+    std::cout << "FAIL " << name << std::endl;
     return;
   }
-  cout << "OK   " << name << endl;
+  std::cout << "OK   " << name << std::endl;
 }
 
 int main() {
