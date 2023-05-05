66039c9ea23bb6c9b82ba82a441920fa272d3971
Zach Philipchook <z.pilipchuk@gmail.com>
2023-05-05 18:13:01 +0300

    add fmt

---
+++ funny/zlisp/builder/fmt.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include <main.h>
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("usage: %s <file>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  char *filename = argv[1];
+  FILE *f = fopen(filename, "r");
+  if (f == NULL) {
+    perror("while opening file (C host)");
+    return EXIT_FAILURE;
+  }
+  read_result rr = datum_read_all(f);
+  fclose(f);
+  if (read_result_is_panic(rr)) {
+    fprintf(stderr, "fmt couldn't parse source: %s\n",
+            rr.panic_message);
+    return EXIT_FAILURE;
+  }
+  assert(read_result_is_ok(rr));
+  assert(datum_is_list(&rr.ok_value));
+  printf("%s\n", datum_repr(&rr.ok_value));
+  return EXIT_SUCCESS;
+}
