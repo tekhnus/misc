92613671bb48f0d47e7b83055e7d3b513be90a78
Zach Philipchook <z.pilipchuk@gmail.com>
2023-05-20 19:09:20 +0300

    fmt

--- funny/zlisp/builder/linking.c
+++ funny/zlisp/builder/linking.c
@@ -5,18 +5,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-EXPORT size_t prog_build_init(vec *sl,
-                              datum *compdata, datum *builder_compdata) {
+EXPORT size_t prog_build_init(vec *sl, datum *compdata,
+                              datum *builder_compdata) {
   size_t e = 0;
   datum nil = datum_make_nil();
   prog_append_yield(sl, &e, datum_make_symbol("halt"), 0, 0, nil,
                     builder_compdata);
   size_t bdr_put_prog = e;
-  prog_append_something(sl, &e);  // filled below
+  prog_append_something(sl, &e); // filled below
   size_t ep_start = e;
   prog_append_yield(sl, &e, datum_make_symbol("plain"), 0, 0, nil, compdata);
   size_t jm = e;
-  prog_append_something(sl, &e);  // filled below
+  prog_append_something(sl, &e); // filled below
   assert(bdr_put_prog + 1 == ep_start);
   *vec_at(sl, bdr_put_prog) = get_put_prog(e - bdr_put_prog, 0, ep_start);
   compdata_put(builder_compdata, datum_make_symbol(":anon"));
@@ -25,7 +25,7 @@ EXPORT size_t prog_build_init(vec *sl,
       datum_make_list_of(compdata_get_top_polyindex(builder_compdata)), false,
       false, datum_make_symbol("plain"), 0, 0, builder_compdata);
   size_t bdr = e;
-  prog_append_something(sl, &e);  // this is first builder instruction.
+  prog_append_something(sl, &e); // this is first builder instruction.
   // filled by prog_build.
   *vec_at(sl, jm) = prog_get_jmp(e - jm);
   return bdr;
@@ -135,10 +135,10 @@ LOCAL char *prog_build_dep(vec *sl, size_t *p, datum *dep_and_sym,
   }
   // a jump is done so that we're at the end of the slice.
   size_t jmp_off = *p;
-  prog_append_something(sl, p);  // filled below
+  prog_append_something(sl, p); // filled below
   *vec_at(sl, jmp_off) = prog_get_jmp(*p - jmp_off);
   size_t put_prog_off = *p;
-  prog_append_something(sl, p);  // filled below
+  prog_append_something(sl, p); // filled below
   size_t prog_off = *p;
   char *er = vec_relocate(sl, p, bc);
   if (er != NULL) {
