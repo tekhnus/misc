c5b37bbde0ec48280510322e41bd8d783c452977
Zach Philipchook <z.pilipchuk@gmail.com>
2023-05-27 20:07:53 +0300

    no more defn

--- funny/zlisp/module/extensions/main.lisp
+++ funny/zlisp/module/extensions/main.lisp
@@ -19,5 +19,5 @@ req
 .fntest.arity := 3
 .backquote := {fn {exp} {if (/std/not- (/std/eq- (/std/type- exp) :list)) {return {list {'quote exp}}} {if (/std/not- exp) {return {list {'list {list {}}}}} {if (/std/eq- (/std/length- exp) 2) {if (/std/eq- (/std/list-at- exp 0) 'tilde) {return (/std/list-at- exp 1)} {return {list {'call {list {'/std/cons (../.backquote (/std/head- exp)) (../.backquote (/std/tail- exp))}}}}}} {return {list {'call {list {'/std/cons (../.backquote (/std/head- exp)) (../.backquote (/std/tail- exp))}}}}}}}}}
 .backquote.arity := 2
-.defnx := {fn {name args body} {return {list {'defn name '{} {list {'mc ':= {list {'magically_called_fn args body}} '{} ':= '(mc @mut @0 @up2) 'return :shouldnt-go-here}} '{} ':= {list {'call {list {name '@mut '@0}}}}}}}}
+.defnx := {fn {name args body} {return {list {name ':= {list {'fn '{} {list {'mc ':= {list {'magically_called_fn args body}} '{} ':= '(mc @mut @0 @up2) 'return :shouldnt-go-here}}}} '{} ':= {list {'call {list {name '@mut '@0}}}}}}}}
 .defnx.arity := 4
