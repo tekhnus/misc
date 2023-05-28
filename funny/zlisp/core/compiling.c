8b3b0fa323ad3bf7250419eaebb127df1be8f04d
Zach Philipchook <z.pilipchuk@gmail.com>
2023-05-28 13:12:16 +0300

    better

--- funny/zlisp/core/compiling.c
+++ funny/zlisp/core/compiling.c
@@ -272,6 +272,7 @@ LOCAL char *prog_append_consume_expression(vec *sl, datum *source, int *i,
     return NULL;
   }
   if (datum_is_list(head)) {
+    // fprintf(stderr, "warning: bare list %s\n", datum_repr(head));
     return prog_append_expressions(sl, head, compdata, ext);
   }
   if (datum_is_constant(head)) {
