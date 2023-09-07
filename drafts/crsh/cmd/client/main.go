5be8744d09a38eced538d2537ababe45b692bcbd
Zach Philipchook <z.pilipchuk@gmail.com>
2023-09-07 20:05:55 +0300

    do

---
+++ drafts/crsh/cmd/client/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"os"
+	"io"
+	"net"
+	// "os/exec"
+	"bufio"
+	"path/filepath"
+
+	"github.com/peterh/liner"
+)
+
+const Addr = "/tmp/crsh.sock"
+
+var (
+	history_fn = filepath.Join(os.TempDir(), ".liner_example_history")
+)
+
+func main() {
+	server, err := net.Dial("unix", Addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer server.Close()
+	reader := bufio.NewReader(server)
+
+	line := liner.NewLiner()
+	defer line.Close()
+
+	line.SetCtrlCAborts(true)
+
+	line.SetTabCompletionStyle(liner.TabPrints)
+	line.SetCompleter(func(line string) (c []string) {
+		// fmt.Fprintln(outfile, "COMPLETE", line)
+		// for {
+		// 	response, isprefix, err := inreader.ReadLine()
+		// 	if isprefix || err != nil {
+		// 		panic("bad")
+		// 	}
+		// 	if len(response) == 0 {
+		// 		break
+		// 	}
+		// 	c = append(c, string(response))
+		// }
+		return
+	})
+
+	if f, err := os.Open(history_fn); err == nil {
+		line.ReadHistory(f)
+		f.Close()
+	}
+
+	// log.Print("Ready")
+	for {
+		// fmt.Printf("\033[9999;1H\x1b[38;5;251m@%s\x1b[K\x1b[0m\033[1;0H", servername)
+		// err = exec.Command("kitty", "@", "focus-window").Run()
+		// if err != nil {
+		// 	panic(err)
+		// }
+		if name, err := line.Prompt("> "); err == nil {
+			server.Write([]byte(name + "\n"))
+			line.AppendHistory(name)
+			if name == "exit" {
+				break
+			}
+			if f, err := os.Create(history_fn); err != nil {
+				log.Print("Error writing history file: ", err)
+			} else {
+				line.WriteHistory(f)
+				f.Close()
+			}
+			// fmt.Print("\033[H\033[2J")
+			// err = exec.Command("kitty", "@", "focus-window", "-m", "cmdline:.*crsh-.*").Run()
+			// if err != nil {
+				// panic(err)
+			// }
+			response, isprefix, err := reader.ReadLine()
+			if err != nil || isprefix {
+				break
+			}
+			if len(response) == 0 {
+				break
+			}
+		} else if err == liner.ErrPromptAborted {
+			fmt.Print("\033[H\033[2J")
+		} else if err == io.EOF {
+			break
+		} else {
+			log.Print("Error reading line: ", err)
+		}
+
+	}
+}
--- drafts/crsh/go.mod
+++ drafts/crsh/go.mod
@@ -3,6 +3,8 @@ module github.com/tekhnus/crsh
 go 1.20
 
 require (
+	github.com/mattn/go-runewidth v0.0.3 // indirect
+	github.com/peterh/liner v1.2.2 // indirect
 	golang.org/x/sync v0.2.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/term v0.8.0 // indirect
