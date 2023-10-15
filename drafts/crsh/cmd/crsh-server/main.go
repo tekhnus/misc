b1c570ae428d6463f6cd074ad2559798c26874ed
Zach Philipchook <z.pilipchuk@gmail.com>
2023-10-15 14:40:42 +0300

    do

--- drafts/crsh/cmd/crsh-server/main.go
+++ drafts/crsh/cmd/crsh-server/main.go
@@ -1,9 +1,56 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello!")
+	if len(os.Args) <= 1 {
+		log.Fatal("Not enough arguments")
+	}
+
+	cmd := os.Args[1]
+	args := os.Args[2:]
+
+	var err error
+
+	switch cmd {
+	case "echo":
+		err = echo(args)
+	default:
+		log.Fatal("Unknown command")
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func echo(args []string) error {
+	listener, err := net.Listen("tcp", "localhost:5678")
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	conn, err := listener.Accept()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	dec := json.NewDecoder(conn)
+	for {
+		var msg map[string]string
+		err = dec.Decode(&msg)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("received: %s\n", msg)
+	}
+	return nil
 }
