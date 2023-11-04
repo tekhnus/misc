fc33910f0ddd51ad723332877227845aee205320
Zach Philipchook <z.pilipchuk@gmail.com>
2023-11-04 13:48:05 +0200

    do

--- drafts/crsh/cmd/crsh/main.go
+++ drafts/crsh/cmd/crsh/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"log"
+	"os"
 )
 
 func main() {
-	log.Println("Hello!")
+	cmd := "manager"
+	args := os.Args[1:]
+
+	if len(args) > 0 {
+		switch args[0] {
+		case "manager", "log-server":
+			cmd = args[0]
+			args = args[1:]
+		}
+	}
+
+	var res error
+	switch cmd {
+	case "manager":
+		res = Manager(args)
+	case "log-server":
+		res = LogServer(args)
+	default:
+		log.Fatal("Unknown command")
+	}
+
+	if res != nil {
+		log.Fatal(res)
+	}
+}
+
+func Manager(args []string) error {
+	return nil
+}
+
+func LogServer(args []string) error {
+	return nil
 }
