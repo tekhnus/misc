fe389ba51db29c84b887bf254dd223e4e8452d87
Zach Philipchook <z.pilipchuk@gmail.com>
2023-11-10 12:09:23 +0200

    do

--- drafts/crsh/cmd/crsh/main.go
+++ drafts/crsh/cmd/crsh/main.go
@@ -406,7 +406,7 @@ func SSHMain(args []string, ctx context.Context) error {
 
 	time.Sleep(5 * time.Second)
 
-	masterExitCmd := exec.Command("ssh", "-M", "-S", masterSocket, "-O", "exit", host)
+	masterExitCmd := exec.Command("ssh", "-S", masterSocket, "-O", "exit", host)
 	defer func() {
 		log.Println("Started command:", masterExitCmd)
 		out, err := masterExitCmd.CombinedOutput()
