bd869abcf9d63b40b35d8aa332a1f4ee863fbe92
Zach Philipchook <z.pilipchuk@gmail.com>
2023-11-18 13:08:35 +0200

    do

--- drafts/crsh/cmd/crsh/main.go
+++ drafts/crsh/cmd/crsh/main.go
@@ -852,7 +852,8 @@ func SSHMain(args []string, ctx context.Context) error {
 		log.Println("Started command:", fwdCmd)
 		var out []byte
 		var err error
-		// out, err = fwdCmd.CombinedOutput()
+		time.Sleep(time.Second * 1) // IDK why
+		out, err = fwdCmd.CombinedOutput()
 		log.Println("!!!!!! RUN THIS:", fwdCmd)
 		log.Println("Finished command:", fwdCmd)
 		log.Print("Output: ", string(out))
